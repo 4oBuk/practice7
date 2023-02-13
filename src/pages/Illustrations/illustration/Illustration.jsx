37eaa9ad3699aad0646e754bca91b00d7cfb5d65
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-02-13 20:10:32 +0200

    rewrite illustrations list using flexboxes

--- src/pages/Illustrations/illustration/Illustration.jsx
+++ src/pages/Illustrations/illustration/Illustration.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 // todo move to components folder
 const getClasses = makeStyles(() => ({
-  table: {
-    width: "50%",
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    order: 4,
+    width: "70%",
   },
-  tableElement: {
-    textAlign: "center",
-  },
+  item: { flexGrow: 1 },
 }));
 const Illustration = ({ illustration }) => {
   const classes = getClasses();
+  const deleteButtonClick = (e, id) => {
+    console.log("delete", id);
+  };
   return (
-    <tr>
-      <td className={classes.tableElement}>{illustration.id}</td>
-      <td className={classes.tableElement}>{illustration.name}</td>
-      <td className={classes.tableElement}>{illustration.aiGenerated ? "yes" : "no"}</td>
-      <Button onClick={editButtonClick}>edit</Button>
-      <Button onClick={deleteButtonClick}>delete</Button>
-    </tr>
+    <div className={classes.container}>
+      <p className={classes.item}>{illustration.id}</p>
+      <p className={classes.item}>{illustration.name}</p>
+      <p className={classes.item}>{illustration.aiGenerated ? "yes" : "no"}</p>
+      <div className={classes.item}>
+        <Link to={`/illustrations/${illustration.id}/edit`}>edit</Link>
+        <Button onClick={(e) => deleteButtonClick(e, illustration.id)}>
+          delete
+        </Button>
+      </div>
+    </div>
   );
 };
-const editButtonClick = () => {};
-const deleteButtonClick = () => {};
 
 export default Illustration;
--- src/pages/Illustrations/index.js
+++ src/pages/Illustrations/index.js
@@ -3,18 +3,17 @@ import Illustration from "./illustration/Illustration";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchIllustrations } from "../../app/actions/illustration";
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 const getClasses = makeStyles(() => ({
   container: {
     display: "flex",
     flexDirection: "row",
+    justifyContent: "center",
+    order: 4,
+    width: "70%",
   },
-  table: {
-    width: "50%",
-  },
-  tableElement: {
-    textAlign: "center",
-  },
+  item: { flexGrow: 1 },
 }));
 
 const Illustrations = () => {
@@ -37,22 +36,21 @@ const Illustrations = () => {
   const ills = useSelector((state) => state.illustrations).illustrations;
   return (
     <>
+      <Link to={"/illustrations/new"}>Add</Link>
       <h2>Illustrations</h2>
-      <table className={classes.table}>
-        <tr>
-          <th className={classes.tableElement}>ID</th>
-          <th className={classes.tableElement}>Name</th>
-          <th className={classes.tableElement}>AI Generated</th>
-          <th className={classes.tableElement}></th>
-        </tr>
-        {state.componentDidMount && (
-          <>
-            {ills.map((i) => (
-              <Illustration illustration={i} key={i.id} />
-            ))}
-          </>
-        )}
-      </table>
+      <div className={classes.container}>
+        <p className={classes.item}>ID</p>
+        <p className={classes.item}>Name</p>
+        <p className={classes.item}>AI Generated</p>
+        <p className={classes.item}></p>
+      </div>
+      {state.componentDidMount && (
+        <>
+          {ills.map((i) => (
+            <Illustration illustration={i} key={i.id} />
+          ))}
+        </>
+      )}
     </>
   );
 };
